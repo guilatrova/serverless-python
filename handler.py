a8e2b4b62db1d96ff0693e5d615957a7cc58f79c
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-08-11 07:18:11 -0300

    fixed bad indentation in handler.py

--- handler.py
+++ handler.py
@@ -1,14 +1,14 @@
 import json
 
-    def retrieve_posts():
-        #Let's imagine that I made a SELECT from some db somewhere
-        return [
-            { "id": 1, "title": "Easy introduction to Redux", "publish_date": "2017-08-01" },
-            { "id": 2, "title": "Serverless", "publish_date": "2017-08-11" },
-        ]
+def retrieve_posts():
+    #Let's imagine that I made a SELECT from some db somewhere
+    return [
+        { "id": 1, "title": "Easy introduction to Redux", "publish_date": "2017-08-01" },
+        { "id": 2, "title": "Serverless", "publish_date": "2017-08-11" },
+    ]
     
-    def posts(event, context):
-        posts = retrieve_posts()
+def posts(event, context):
+    posts = retrieve_posts()
 
     body = {
         "message": "Nice job! Your function executed successfully!",
